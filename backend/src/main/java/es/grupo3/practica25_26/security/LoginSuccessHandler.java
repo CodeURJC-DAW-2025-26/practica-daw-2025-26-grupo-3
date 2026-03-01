e8020b300be5880dc0a08d9db9c3a68c96867db4
MarcosHM851 <m.hernandezm.2023@alumnos.urjc.es>
2026-03-01 14:14:30 +0100

    If user is anonymous and visit a logged user URL and logs in, now the app goes to the page the user wanted to go instead of index

--- backend/src/main/java/es/grupo3/practica25_26/security/LoginSuccessHandler.java
+++ backend/src/main/java/es/grupo3/practica25_26/security/LoginSuccessHandler.java
@@ -3,7 +3,7 @@ package es.grupo3.practica25_26.security;
 import java.io.IOException;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.FlashMap;
 import org.springframework.web.servlet.FlashMapManager;
@@ -14,7 +14,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
