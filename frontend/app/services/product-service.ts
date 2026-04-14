a70b3eaebc62a5251880d851cca8b02b2466a992
DanielHernanz9 <dhernanz349@gmail.com>
2026-04-14 10:51:28 +0200

    Added productBasicDto, ImageDTO and product-service.tsx. I moved product tsx files to the product folder so that it is better organized. Also I added the react-boostrap library

---
+++ frontend/app/services/product-service.ts
@@ -0,0 +1,123 @@
+import type { ProductBasicDTO } from "~/dtos/ProductBasicDTO";
+
+
+const API_URL = "/api/v1/products";
+const API_IMAGES_URL = "/api/v1/images";
+
+export async function getProducts(): Promise<ProductBasicDTO[]> {
+    const res = await fetch(`${API_URL}/`);
+    return await res.json();
+}
+
+export async function getBasicProduct(id: number): Promise<ProductBasicDTO> {
+    const res = await fetch(`${API_URL}/${id}`);
+    if (!res.ok) {
+        throw new Error("Product not found");
+    }
+    return await res.json();
+}
+
+export async function addBasicProduct(
+    productName: string,
+    description: string,
+    price: number,
+    state: number
+): Promise<ProductBasicDTO> {
+    const response = await fetch(`${API_URL}/`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            productName,
+            description,
+            price,
+            state
+        }),
+    });
+
+    if (!response.ok) {
+        throw new Error("Error adding product");
+    }
+
+    return await response.json();
+}
+
+export async function removeProduct(id: number): Promise<void> {
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: "DELETE",
+    });
+
+    if (!response.ok) {
+        throw new Error("Error removing product");
+    }
+}
+
+export async function updateProduct(
+    id: number,
+    productName: string,
+    description: string,
+    price: number,
+    state: number
+): Promise<ProductBasicDTO> {
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ productName, description, price, state }),
+    });
+
+    if (!response.ok) {
+        throw new Error("Error updating product");
+    }
+
+    return await response.json();
+}
+
+
+export async function uploadProductImage(
+    id: number,
+    imageFile: File,
+): Promise<void> {
+    const formData = new FormData();
+    formData.append("imageFile", imageFile);
+
+    const response = await fetch(`${API_URL}/${id}/images/`, {
+        method: "POST",
+        body: formData,
+    });
+
+    if (!response.ok) {
+        throw new Error("Error uploading image");
+    }
+}
+
+export async function deleteProductImage(
+    bookId: number,
+    imageId: number,
+): Promise<void> {
+    const response = await fetch(`${API_URL}/${bookId}/images/${imageId}`, {
+        method: "DELETE",
+    });
+
+    if (!response.ok) {
+        throw new Error("Error deleting image");
+    }
+}
+
+export async function replaceImage(
+    imageId: number,
+    imageFile: File,
+): Promise<void> {
+    const formData = new FormData();
+    formData.append("imageFile", imageFile);
+
+    const response = await fetch(`${API_IMAGES_URL}/${imageId}/media`, {
+        method: "PUT",
+        body: formData,
+    });
+
+    if (!response.ok) {
+        throw new Error("Error replacing image");
+    }
+}
+
+
+
