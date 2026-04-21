598a8567e0586783f1406220bf23c936f48af8f7
DanielHernanz9 <dhernanz349@gmail.com>
2026-04-21 10:47:16 +0200

    Added product detail page with edit and remove buttons only visible to admin or owners. Also now is visible the review form to add reviews to products.
    Delete product button is completely functional, and also index redirects to login page if the user isn´t logged

--- frontend/app/dtos/ProductDetailDTO.ts
+++ frontend/app/dtos/ProductDetailDTO.ts
@@ -1,10 +1,13 @@
+import type { UserBasicDTO } from "./UserBasicDTO";
+
 export interface ProductDetailDTO {
-    id: number;
-    productName: string;
-    price: number;
-    state: number;
-    description: string;
-    averageRating: number;
-    reviewCount: number;
-    images: { id: number }[];
+    id: number,
+    productName: string,
+    price: number,
+    state: number,
+    description: string,
+    averageRating: number,
+    reviewCount: number,
+    images: { id: number }[],
+    seller: UserBasicDTO
 }
